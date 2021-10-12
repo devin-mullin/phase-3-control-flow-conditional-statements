719fd7e3e5628d2f64107b1a1870d30b85dfc372
Devin Mullin <devin.mullin90@gmail.com>
2021-10-12 16:43:44 -0500

    commit tree efcce4ee74c89ca09e4a9732474ba3236878f36a

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,31 @@
+require 'pry'
+
 def admin_login(username, password)
-  # your code here
+  
+  if username = "admin" || username = "ADMIN" && password = "12345"
+    puts "Access granted"
+  else 
+    puts "Access denied"
 end
+
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40
+    puts "Brisk!"
+  elsif temperature > 39 && <= 65
+    puts "It's a little chilly out there!"
+  elsif temperature > 85 
+    puts "It's too dang hot out there!"
+  else "It's perfect out there!"
 end
 
-def fizzbuzz(num)
-  # your code here
-end
 
-def calculator(operation, num1, num2)
-  # your code here
+# def fizzbuzz(num)
+#   # your code here
+# end
+
+# def calculator(operation, num1, num2)
+#   # your code here
+# end
 end
 
